5db1e71061af3dd85475069e13bc7685560416b0
Mayank Mathur <Mynk-9@users.noreply.github.com>
2017-01-20 23:48:13 +0530

    Small update. Placeholders even now.

--- algo.h
+++ algo.h
@@ -1,13 +1,29 @@
 /**
-* (c) Mayank Mathur
-* see link -- https://github.com/Mynk-9/Ninth-Sky/blob/master/LICENSE
+
+Copyright 2017 Mayank Mathur
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
 */
 
 #ifndef ALGORITHMS_9THSKY
 #define ALGORITHMS_9THSKY
 
 
-/**	code goes here */
+namespace ninth_sky
+{
+	/// code goes here
+}
 
 
 #endif
